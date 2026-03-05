ca9cbc9fb3b5964adbb72828b5ca4ef79000c23e
gerackl <stroev20160@gmail.com>
2026-03-05 23:20:55 +0000

    test: test typer cli

--- pfcookiecutter/cli.py
+++ pfcookiecutter/cli.py
@@ -7,6 +7,10 @@ def hello(name: str):
     """Say hello to someone"""
     typer.echo(f"Hello {name}")
 
+@app.command()
+def goodbye(name: str):
+    typer.echo(f"Goodbye {name}")
+
 
 if __name__ == "__main__":
     app()

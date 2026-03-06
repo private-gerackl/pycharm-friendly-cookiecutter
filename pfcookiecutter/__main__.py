745f536783afb2e8fd0b6b3ceec3762f082bd60e
gstroev <g.stroev@fssoft.ru>
2026-03-06 18:03:25 +0300

    temp

--- pfcookiecutter/__main__.py
+++ pfcookiecutter/__main__.py
@@ -5,7 +5,19 @@ from pfcookiecutter.pfc import pfc
 pfc(
     template_path=Path().parent / 'pfcookiecutter' / 'templates' / 'postgres_template',
     template_folder='_11source_name11_',
+
 )
 
 class PostgreSQLTemplateBuilder:
-    pass
+    def __init__(
+        self,
+        template_path: Path,
+        template_folder: str,
+    ):
+        self._template_path = template_path
+        self._template_folder = template_folder
+
+    def pfc(
+        self,
+
+    ):

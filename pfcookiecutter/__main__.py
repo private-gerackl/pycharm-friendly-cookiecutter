a753aaec8b04e934573aa4950a0a3a9f1d64a0e2
gerackl <stroev20160@gmail.com>
2026-03-09 12:01:59 +0000

    feat: add AppBuilderFactory class

--- pfcookiecutter/__main__.py
+++ pfcookiecutter/__main__.py
@@ -5,41 +5,65 @@ from pfcookiecutter.synthesizers.synthesizers.postgres_infra_template.models imp
 from pfcookiecutter.synthesizers.synthesizers.postgres_infra_template.synthesizer import PostgresInfraSynthesizer
 
 
-def app_builder_factory(
-    source_name: str = 'app',
-    integration_layer_name: str = 'integrations',
-    service_layer_name: str = 'services',
-) -> SynthesizerBuilder:
-    return SynthesizerBuilder(
-        SynthesizerBuilder(
+class AppBuilderFactory:
+
+    def __init__(
+        self,
+        source_name: str = 'app',
+        integration_layer_name: str = 'integrations',
+        service_layer_name: str = 'services',
+    ):
+        self.source_name = source_name
+        self.integration_layer_name = integration_layer_name
+        self.service_layer_name = service_layer_name
+
+    def build(self) -> SynthesizerBuilder:
+        return SynthesizerBuilder(
+            self.integration_builder,
+            self.service_builder,
+        )
+
+    @property
+    def integration_builder(self) -> SynthesizerBuilder:
+        return SynthesizerBuilder(
             LayerSynthesizer(
                 content=LayerSynthesizerContent(
-                    source_name=source_name,
-                    layer_name=integration_layer_name,
-                    LayerName=integration_layer_name.capitalize()[:-1],
+                    source_name=self.source_name,
+                    layer_name=self.integration_layer_name,
+                    LayerName=self.integration_layer_name_camel_case,
                 )
             ),
             PostgresInfraSynthesizer(
                 content=PostgresInfraSynthesizerContent(
-                    source_name=source_name,
+                    source_name=self.source_name,
                     integration_name='postgresql',
                     IntegrationName='PSQL',
-                    BaseIntegrationClass=f'Base{integration_layer_name.capitalize()[:-1]}',
-                    base_integration_class_module=f'{source_name}.{integration_layer_name}.base',
+                    BaseIntegrationClass=f'Base{self.integration_layer_name.capitalize()[:-1]}',
+                    base_integration_class_module=f'{self.source_name}.{self.integration_layer_name}.base',
                 )
             ),
-        ),
-        SynthesizerBuilder(
+        )
+
+    @property
+    def service_builder(self) -> SynthesizerBuilder:
+        return SynthesizerBuilder(
             LayerSynthesizer(
                 content=LayerSynthesizerContent(
-                    source_name=source_name,
-                    layer_name=service_layer_name,
-                    LayerName=service_layer_name.capitalize()[:-1],
+                    source_name=self.source_name,
+                    layer_name=self.service_layer_name,
+                    LayerName=self.service_layer_name_camel_case,
                 )
             )
         )
-    )
+
+    @property
+    def integration_layer_name_camel_case(self) -> str:
+        return self.integration_layer_name.capitalize()[:-1]
+
+    @property
+    def service_layer_name_camel_case(self) -> str:
+        return self.service_layer_name.capitalize()[:-1]
 
 
-app_builder = app_builder_factory()
-app_builder.pfc()
+app_builder = AppBuilderFactory()
+app_builder.build().pfc()
